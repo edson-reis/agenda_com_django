c4a5f99c1e66ede417d1ab7e83024179b28e0787
Edson Reis <elias.edson@gmail.com>
2020-05-12 17:11:58 -0300

    Criaçao da Tabela de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from core.models import ListaEvento
 # Register your models here.
+
+class ListaEventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('titulo','usuario','data_evento',)
+
+admin.site.register(ListaEvento, ListaEventoAdmin)
+
