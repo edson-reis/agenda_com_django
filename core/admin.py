73b713dd6772f534f18e23666f0004e65e6fc17b
Edson Reis <elias.edson@gmail.com>
2020-05-13 21:13:32 -0300

    Criadas as funções de edição e exclusao dos registros

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from core.models import ListaEvento
 # Register your models here.
 
 class ListaEventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo','data_evento','data_criacao')
+    list_display = ('id','titulo','data_evento','data_criacao')
     list_filter = ('titulo','usuario','data_evento',)
 
 admin.site.register(ListaEvento, ListaEventoAdmin)
--- core/views.py
+++ core/views.py
@@ -37,7 +37,11 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render (request,'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = ListaEvento.objects.get(id=id_evento)
+    return render (request,'evento.html', dados)
 
 @login_required(login_url='/login/')
 def submit_evento(request):
@@ -46,9 +50,23 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        ListaEvento.objects.create(titulo=titulo,
-                                   descricao=descricao,
-                                   data_evento=data_evento,
-                                   usuario=usuario)
-        return redirect('/')
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            ListaEvento.objects.filter(id=id_evento).update(titulo=titulo,
+                                                            descricao=descricao,
+                                                            data_evento=data_evento)
+        else:
+            ListaEvento.objects.create(titulo=titulo,
+                                       descricao=descricao,
+                                       data_evento=data_evento,
+                                       usuario=usuario)
+            return redirect('/')
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = ListaEvento.objects.get(id=id_evento)
+    if (usuario==evento.usuario):
+        evento.delete()
     return redirect('/')
